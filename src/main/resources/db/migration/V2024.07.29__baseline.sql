2ac038c3da0b6d66ff44e24cd05d2b5f92cebe3e
gabimarginean <gabimarginean@hotmail.com>
2024-07-31 09:05:40 +0300

    Created database structure

---
+++ src/main/resources/db/migration/V2024.07.29__baseline.sql
@@ -0,0 +1,48 @@
+CREATE TABLE user
+(
+    id            BIGINT PRIMARY KEY AUTO_INCREMENT,
+    username      VARCHAR(255),
+    password      VARCHAR(255),
+    role          VARCHAR(25),
+    created       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated       TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE product_category
+(
+    id            BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name          VARCHAR(50) NOT NULL,
+    created       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated       TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE warehouse
+(
+    id            BIGINT PRIMARY KEY AUTO_INCREMENT,
+    address       VARCHAR(255) NOT NULL,
+    capacity      BIGINT NOT NULL,
+    created       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated       TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE product
+(
+    id            BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name          VARCHAR(50) NOT NULL,
+    description   VARCHAR(255),
+    price         DOUBLE NOT NULL,
+    category_id   BIGINT,
+    created       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated       TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES ProductCategory (id)
+);
+
+CREATE TABLE product_warehouse
+(
+    product_id     BIGINT,
+    warehouse_id   BIGINT,
+    quantity       BIGINT,
+    PRIMARY KEY (product_id, warehouse_id),
+    FOREIGN KEY (product_id) REFERENCES Product (id),
+    FOREIGN KEY (warehouse_id) REFERENCES Warehouse (id)
+);
