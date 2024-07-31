1c976d8566e4487942f3e139e9543a40b78baf1d
gabimarginean <gabimarginean@hotmail.com>
2024-07-31 08:29:51 +0300

    Created project structure and UML diagram

---
+++ src/main/java/com/ing/IngApplication.java
@@ -0,0 +1,21 @@
+package com.ing;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class IngApplication {
+    private static final Logger logger
+            = LoggerFactory.getLogger(IngApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngApplication.class, args);
+        logger.info("App started");
+    }
+}
+
+
